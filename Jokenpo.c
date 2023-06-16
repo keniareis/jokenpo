f933d17aeeaaedf19495e8927e267a9ce9bd141e
Kenia de Oliveira <keniapubg@gmail.com>
2023-06-16 08:36:18 -0300

    Corrigindo erros

--- Jokenpo.c
+++ Jokenpo.c
@@ -12,11 +12,11 @@ int main() {
   printf("Escolha: \nPedra[1]\n \npapel[2]\n \ntesoura[3]\n ");
   scanf("%d", &Usuario);
   
-  maquina = rand() % 4;// sorteia um número de 0 a 2
+  maquina = (rand() % 3)+1;// sorteia um número de 0 a 2
   
   printf("A maquina escolheu %d\n", maquina);
   printf("Voce escolheu %d\n", Usuario);
-  r= maquina-Usuario;
+  r= Usuario-maquina;
 
   if(r==0){
     printf("Empate");
@@ -24,8 +24,11 @@ int main() {
   else if(r==1){
       printf("Voce ganhou!");
     }
-    else{
+    else
+    {   if (r>1)
+        {
         printf("O computador ganhou");
+        }
     }
   
   return 0;
